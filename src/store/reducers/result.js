87213c694eef79ef3c7ac602b65a409d89318365
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-07 20:26:08 +0700

    Leaner switch case

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,19 +5,16 @@ const initialState = {
   results: []
 }
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+  return updateObject(state, { results: updatedArray })
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
 
-    case actionType.STORE_RESULT:
-      // changed data
-      return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) })
-
-    case actionType.DELETE_RESULT:
-      // const id = 2
-      // const newArray = [...state.results]
-      // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-      return updateObject(state, { results: updatedArray })
+    case actionType.STORE_RESULT: return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) })
+    case actionType.DELETE_RESULT: return deleteResult(state, action)
   }
   return state
 }
