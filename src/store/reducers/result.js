02102299c333433ce9306ce1dc6456a57f9d3026
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-07 19:58:35 +0700

    Using utility function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionType from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
   results: []
@@ -9,19 +10,14 @@ const reducer = (state = initialState, action) => {
 
     case actionType.STORE_RESULT:
       // changed data
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.result * 2 })
-      }
+      return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) })
+
     case actionType.DELETE_RESULT:
       // const id = 2
       // const newArray = [...state.results]
       // newArray.splice(id, 1)
       const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-      return {
-        ...state,
-        results: updatedArray
-      }
+      return updateObject(state, { results: updatedArray })
   }
   return state
 }
